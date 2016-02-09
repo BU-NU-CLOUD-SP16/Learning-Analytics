35625f096cfa41c810be1002c8eafc1f785c0e0d
Ubuntu <ubuntu@ip-172-31-37-229.us-west-2.compute.internal>
2016-02-09 00:48:17 +0000

    Add endpoint to test DB data

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var databaseConn = mysql.createConnection({
 	host: "localhost",
     	user: "root",
     	password: "",
-	database: "test",
+	database: "demo1",
 });
 
 databaseConn.connect(function (err){
@@ -63,12 +63,26 @@ app.get('/assignments', function(req, res) {
 
 });
 
+// Return all info about problem based on id
 app.get('/problem/:problem_id', function(req, res) {
 	var problem_id = req.params.problem_id;
 	databaseConn.query('SELECT * FROM problem WHERE id = ' + problem_id, function (err, problem_title){
 		if(err) throw err;
 		console.log('Data received from DB');
 		res.send(problem_title);
+	});
+});
+
+// Return specific information related to the probelm
+app.get('/problem/:problem_id/:modifier', function(req, res) {
+	var problem_id = req.params.problem_id;
+	var modifier = req.params.modifier;
+
+	// Query DB
+	databaseConn.query('SELECT ' + modifier + ' FROM problem WHERE id = ' + problem_id, function (err, problem_specific_info){
+		if(err) throw err;
+		console.log('Data received from DB');
+		res.send(problem_specific_info);
 	});
 });
 
