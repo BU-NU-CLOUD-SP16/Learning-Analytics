815a0531ca0ef5f501658ea4a70bac97b1f9917c
ryanoflaherty <ryflare@bu.edu>
2016-02-15 12:05:27 -0500

    basic error handling

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var databaseConn = mysql.createConnection({
 	host: "localhost",
     	user: "root",
     	password: "",
-	database: "test",
+	database: "demo1",
 });
 
 databaseConn.connect(function (err){
@@ -67,12 +67,32 @@ app.get('/assignments', function(req, res) {
 app.get('/problem/:problem_id', function(req, res) {
 	var problem_id = req.params.problem_id;
 	databaseConn.query('SELECT * FROM problem WHERE id = ' + problem_id, function (err, problem_title){
-		if(err) throw err;
+		if(err)
+      console.log(err);
+    else {
 		console.log('Data received from DB');
 		res.send(problem_title);
+  }
 	});
 });
 
+// Return specific information related to the probelm
+app.get('/problem/:problem_id/:modifier', function(req, res) {
+  var problem_id = req.params.problem_id;
+  var modifier = req.params.modifier;
+
+  // Query DB
+  databaseConn.query('SELECT ' + modifier + ' FROM problem WHERE id = ' + problem_id, function (err, problem_specific_info){
+    if(err) {
+      console.log(err);
+      res.status(500).send({status:500, message: 'internal error', type:'internal'});
+    }
+    else {
+      console.log('Data received from DB');
+    res.send(problem_specific_info);
+  }
+  });
+});
 
 /*
 app.post('/api/comments', function(req, res) {
