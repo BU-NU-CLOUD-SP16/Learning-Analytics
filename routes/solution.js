17a5da7a87350cd870ad91ccbdfd71ae5c71d86f
ryanoflaherty <ryflare@bu.edu>
2016-02-22 17:59:18 -0500

    distribution of data

--- routes/solution.js
+++ routes/solution.js
@@ -13,7 +13,20 @@ app.get('/student/metric', function (req, res) {
 	});
 });
 
-app.get('/student/metric/:id', function (req, res) {
+app.get('/student/metric/bins', function (req, res) {
+  	databaseConn.query('SELECT player_id, metric FROM solution;', function (err, rows){
+		if(err) {
+          	console.log(err);
+          	res.status(500).send({status:500, message: 'internal error', type:'internal'});
+        } else {
+        	console.log('Data received from DB');
+        	var data = parseBinData(rows)
+			res.send(data);
+		}
+	});
+});
+
+app.get('/student/metrics/:id', function (req, res) {
     databaseConn.query('SELECT player_id, metric FROM solution WHERE player_id=' + req.params.id, function (err, rows){
 		if(err) {
           		console.log(err);
@@ -30,19 +43,42 @@ app.get('/student/metric/:id', function (req, res) {
 }
 
 function parseBarData (reqObject){
-
-	var bins = 5;
 	var values = [{"x": '', "y": 0},];
+	var name = "barData";
+	
 	for (i = 0; i < reqObject.length; i++){
 		values[i] = {"x": reqObject[i].player_id, "y": reqObject[i].metric};
 	}
 
-	return buildBarDataObject(values);
+	var meta = [{"count": i, "bins": null}]
+
+	return buildBarDataObject(values, meta, name);
 };
 
-function buildBarDataObject (values){
+function parseBinData (reqObject){
+	var bins = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+	var values = [{"x": '', "y": 0}];
+	var temp = 0;
+	var name = "binData";
+	
+	for (i = 0; i < reqObject.length; i++){
+		temp = Math.floor(reqObject[i].metric/10);
+		bins[temp] += 1;
+	}
+
+	for (j = 0; j < bins.length; j++){
+		values[j] = {"x": j*10, "y": bins[j]};
+	}
+
+	var meta = [{"count": i, "bins": j}]
+
+	return buildBarDataObject(values, meta, name);
+}
+
+function buildBarDataObject (values, metadata, name){
 	var barData = [{
-  	"name": "barData",
+	"meta": metadata,
+  	"name": name,
   	"values": values,
 	}];
   return barData;
