856939ca088a60c9e5f7b4082ed72ee8e50474d9
ryanoflaherty <ryflare@bu.edu>
2016-03-14 16:03:01 -0400

    endpoint to return all problems

--- routes/problem.js
+++ routes/problem.js
@@ -1,5 +1,19 @@
 module.exports = function(app, databaseConn) {
 
+    // Return all info about all problems
+    app.get('/problems', function(req, res) {
+      var problem_id = req.params.problem_id;
+      databaseConn.query('SELECT * FROM problem', function (err, problems){
+        if(err) {
+          console.log(err);
+          res.status(500).send({status:500, message: 'internal error', type:'internal'});
+        } else {
+        console.log('Data received from DB');
+        res.send(problems);
+      }
+      });
+    });
+
     // Return all info about problem based on id
     app.get('/problem/:problem_id', function(req, res) {
       var problem_id = req.params.problem_id;
